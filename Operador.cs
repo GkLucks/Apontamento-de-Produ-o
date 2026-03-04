da0f65a301c458f34780f113b884bc882c9f984e
Lucas <50891526+GkLucks@users.noreply.github.com>
2026-03-04 10:42:57 -0300

    Add Operador class with properties and constructor

---
+++ Operador.cs
@@ -0,0 +1,18 @@
+
+public class Operador : Base
+{
+    public string Usuario { get; set; }
+    //O que usar na senha ?
+    public string Senha {get; set;}
+    //ativa e ativo são iguais ?
+    public bool Ativa { get; set; }
+
+    // Construtor opcional
+    public Operador(int id, string nome, string senha, bool ativa)
+    {
+        Id = id;
+        Nome = nome;
+        Senha = senha;
+        Ativa = ativa;
+    }
+}
